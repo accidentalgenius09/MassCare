48aeb1a4620e35e8b57744b38fcab83c89ec4c0b
surjith-netroxe <surjith.k@netroxe.com>
2025-10-17 23:57:30 +0530

    privacy policy completed

---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,315 @@
+import PageBanner from "@/components/sections/Common/PageBanner";
+import TTSWrapper from "@/hooks/TTSWrapper";
+import React from "react";
+
+function PrivacyPolicy() {
+  return (
+    <>
+      <PageBanner
+        title="Privacy Policy"
+        breadcrumb="Home / Privacy Policy"
+        image="/common/privacypolicy-banner.png"
+        description="Lorem Ipsum 8 years of meaningful care... care without compromise."
+      />
+      <div className="px-14 pt-10 pb-15">
+        <div>
+          <h1 className="text-3xl font-bold">
+            <TTSWrapper text="Privacy Policy">Privacy Policy</TTSWrapper>
+          </h1>
+          <p className="text-xs mt-2">
+            <TTSWrapper
+              text="This Privacy Policy governs the manner in which the Operators’s web
+            sites collects, uses, maintains and discloses information collected
+            from users of this website. This privacy policy applies to the Site
+            and all products and services offered by the Operators’s web sites."
+            >
+              This Privacy Policy governs the manner in which the Operators’s
+              web sites collects, uses, maintains and discloses information
+              collected from users of this website. This privacy policy applies
+              to the Site and all products and services offered by the
+              Operators’s web sites.
+            </TTSWrapper>
+          </p>
+        </div>
+        <div className="mt-8">
+          <h3 className="font-semibold">
+            <TTSWrapper text="Personal identification information">
+              Personal identification information
+            </TTSWrapper>
+          </h3>
+          <p className="text-xs mt-2">
+            <TTSWrapper
+              text="We may collect personal identification information from Users in a
+                variety of ways, including, but not limited to, when Users visit our
+                site, register on the site, place an order, subscribe to the
+                newsletter, respond to a survey, fill out a form, and in connection
+                with other activities, services, features or resources we make
+                available on our Site. Users may be asked for, as appropriate, name,
+                email address. We will collect personal identification information
+                from Users only if they voluntarily submit such information to us.
+                Users can always refuse to supply
+                personally identification information, except that it may prevent
+                them from engaging in certain Site related activities."
+            >
+              We may collect personal identification information from Users in a
+              variety of ways, including, but not limited to, when Users visit
+              our site, register on the site, place an order, subscribe to the
+              newsletter, respond to a survey, fill out a form, and in
+              connection with other activities, services, features or resources
+              we make available on our Site. Users may be asked for, as
+              appropriate, name, email address. We will collect personal
+              identification information from Users only if they voluntarily
+              submit such information to us. Users can always refuse to supply
+              personally identification information, except that it may prevent
+              them from engaging in certain Site related activities.
+            </TTSWrapper>
+          </p>
+        </div>
+        <div className="mt-8">
+          <h3 className="font-semibold">
+            <TTSWrapper text="Non-personal identification information">
+              Non-personal identification information
+            </TTSWrapper>
+          </h3>
+          <p className="text-xs mt-2">
+            <TTSWrapper
+              text="We may collect non-personal identification information about Users
+            whenever they interact with our Site. Non-personal identification
+            information may include the browser name, the type of computer and
+            technical information about Users means of connection to our Site,
+            such as the operating system and the Internet service providers
+            utilized and other similar information."
+            >
+              We may collect non-personal identification information about Users
+              whenever they interact with our Site. Non-personal identification
+              information may include the browser name, the type of computer and
+              technical information about Users means of connection to our Site,
+              such as the operating system and the Internet service providers
+              utilized and other similar information.
+            </TTSWrapper>
+          </p>
+        </div>
+        <div className="mt-8">
+          <h3 className="font-semibold">
+            <TTSWrapper text="Web browser cookies">
+              Web browser cookies
+            </TTSWrapper>
+          </h3>
+          <p className="text-xs mt-2">
+            <TTSWrapper
+              text="Our Site may use “cookies” to enhance User experience. User’s web
+            browser places cookies on their hard drive
+            for record-keeping purposes and sometimes to track information about
+            them. User may choose to set their web browser to refuse cookies, or
+            to alert you when cookies are being sent. If they do so, note that
+            some parts of the Site may not function properly."
+            >
+              Our Site may use “cookies” to enhance User experience. User’s web
+              browser places cookies on their hard drive
+              for record-keeping purposes and sometimes to track information
+              about them. User may choose to set their web browser to refuse
+              cookies, or to alert you when cookies are being sent. If they do
+              so, note that some parts of the Site may not function properly.
+            </TTSWrapper>
+          </p>
+        </div>
+        <div className="mt-8">
+          <h3 className="font-semibold">
+            <TTSWrapper text="How we use collected information">
+              How we use collected information
+            </TTSWrapper>
+          </h3>
+          <div className="text-xs mt-2">
+            <TTSWrapper
+              text="The Operators’s web sites may collect and use Users personal
+                information for the following purposes:"
+            >
+              The Operators’s web sites may collect and use Users personal
+              information for the following purposes:
+            </TTSWrapper>
+            <ol className="list-alpha-paren list-inside mt-2">
+              <li className="py-2">
+                <TTSWrapper
+                  text="To improve customer service - Information you provide helps us
+                        respond to your customer service requests and support needs more
+                        efficiently."
+                >
+                  To improve customer service - Information you provide helps us
+                  respond to your customer service requests and support needs
+                  more efficiently.
+                </TTSWrapper>
+              </li>
+              <li className="py-2">
+                <TTSWrapper
+                  text="To personalize user experience - We may use information in the
+                    aggregate to understand how our Users as a group use the
+                    services and resources provided on our Site."
+                >
+                  To personalize user experience - We may use information in the
+                  aggregate to understand how our Users as a group use the
+                  services and resources provided on our Site.
+                </TTSWrapper>
+              </li>
+              <li className="py-2">
+                <TTSWrapper
+                  text="To improve our Site - We may use feedback you provide to improve
+                    our products and services."
+                >
+                  To improve our Site - We may use feedback you provide to
+                  improve our products and services.
+                </TTSWrapper>
+              </li>
+              <li className="py-2">
+                <TTSWrapper
+                  text="We may use the information Users provide about themselves when
+                    placing an order only to provide service to that order. We do
+                    not share this information with outside parties except to the
+                    extent necessary to provide the service."
+                >
+                  We may use the information Users provide about themselves when
+                  placing an order only to provide service to that order. We do
+                  not share this information with outside parties except to the
+                  extent necessary to provide the service.
+                </TTSWrapper>
+              </li>
+              <li className="py-2">
+                <TTSWrapper
+                  text="To run a promotion, contest, survey or other Site feature - To
+                    send Users information they agreed to receive about topics we
+                    think will be of interest to them."
+                >
+                  To run a promotion, contest, survey or other Site feature - To
+                  send Users information they agreed to receive about topics we
+                  think will be of interest to them.
+                </TTSWrapper>
+              </li>
+              <li className="py-2">
+                <TTSWrapper
+                  text="We may use the email address to send User information and
+                    updates pertaining to their order. It may also be used to
+                    respond to their inquiries, questions, and/or other requests. If
+                    User decides to opt-in to our mailing list, they will receive
+                    emails that may include company news, updates, related product
+                    or service information, etc. If at any time the User would like
+                    to unsubscribe from receiving future emails, we include detailed
+                    unsubscribe instructions at the bottom of each email."
+                >
+                  We may use the email address to send User information and
+                  updates pertaining to their order. It may also be used to
+                  respond to their inquiries, questions, and/or other requests.
+                  If User decides to opt-in to our mailing list, they will
+                  receive emails that may include company news, updates, related
+                  product or service information, etc. If at any time the User
+                  would like to unsubscribe from receiving future emails, we
+                  include detailed unsubscribe instructions at the bottom of
+                  each email.
+                </TTSWrapper>
+              </li>
+            </ol>
+          </div>
+        </div>
+        <div className="mt-8">
+          <h3 className="font-semibold">
+            <TTSWrapper text="How we protect your information">
+              How we protect your information
+            </TTSWrapper>
+          </h3>
+          <p className="text-xs mt-2">
+            <TTSWrapper
+              text="We adopt appropriate data collection, storage and processing
+                practices and security measures to protect against unauthorized
+                access, alteration, disclosure or destruction of your personal
+                information, username, password, transaction information and data
+                stored on our Site. Sensitive and private data exchange between the
+                Site and its Users happens over a SSL secured communication channel
+                and is encrypted and protected with digital signatures."
+            >
+              We adopt appropriate data collection, storage and processing
+              practices and security measures to protect against unauthorized
+              access, alteration, disclosure or destruction of your personal
+              information, username, password, transaction information and data
+              stored on our Site. Sensitive and private data exchange between
+              the Site and its Users happens over a SSL secured communication
+              channel and is encrypted and protected with digital signatures.
+            </TTSWrapper>
+          </p>
+        </div>
+        <div className="mt-8">
+          <h3 className="font-semibold">
+            <TTSWrapper text="Sharing your personal information">
+              Sharing your personal information
+            </TTSWrapper>
+          </h3>
+          <p className="text-xs mt-2">
+            <TTSWrapper
+              text="We do not sell, trade, or rent Users
+                personal identification information to others. We may share generic
+                aggregated demographic information not linked to any
+                personal identification information regarding visitors and users
+                with our business partners, trusted affiliates and advertisers for
+                the purposes outlined above."
+            >
+              We do not sell, trade, or rent Users
+              personal identification information to others. We may share
+              generic aggregated demographic information not linked to any
+              personal identification information regarding visitors and users
+              with our business partners, trusted affiliates and advertisers for
+              the purposes outlined above.
+            </TTSWrapper>
+          </p>
+        </div>
+        <div className="mt-8">
+          <h3 className="font-semibold">
+            <TTSWrapper text="Changes to this privacy policy">
+              Changes to this privacy policy
+            </TTSWrapper>
+          </h3>
+          <p className="text-xs mt-2">
+            <TTSWrapper
+              text="Xpert Driving Tuition has the discretion to update this privacy
+                policy at any time. When we do, we will post a notification on the
+                main page of our Site, revise the updated date at the bottom of this
+                page and send you an email. We encourage Users to frequently check
+                this page for any changes to stay informed about how we are helping
+                to protect the personal information we collect. You acknowledge and
+                agree that it is your responsibility to review this privacy policy
+                periodically and become aware of modifications."
+            >
+              Xpert Driving Tuition has the discretion to update this privacy
+              policy at any time. When we do, we will post a notification on the
+              main page of our Site, revise the updated date at the bottom of
+              this page and send you an email. We encourage Users to frequently
+              check this page for any changes to stay informed about how we are
+              helping to protect the personal information we collect. You
+              acknowledge and agree that it is your responsibility to review
+              this privacy policy periodically and become aware of
+              modifications.
+            </TTSWrapper>
+          </p>
+        </div>
+        <div className="mt-8">
+          <h3 className="font-semibold">
+            <TTSWrapper text="Your acceptance of these terms">
+              Your acceptance of these terms
+            </TTSWrapper>
+          </h3>
+          <p className="text-xs mt-2">
+            <TTSWrapper
+              text="By using this Site, you signify your acceptance of this policy. If
+                you do not agree to this policy, please do not use our Site. Your
+                continued use of the Site following the posting of changes to this
+                policy will be deemed your acceptance of those changes."
+            >
+              By using this Site, you signify your acceptance of this policy. If
+              you do not agree to this policy, please do not use our Site. Your
+              continued use of the Site following the posting of changes to this
+              policy will be deemed your acceptance of those changes.
+            </TTSWrapper>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default PrivacyPolicy;
