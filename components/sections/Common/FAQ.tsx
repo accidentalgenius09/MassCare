4a901eca2fe0f433809809d3565bc18a85be327e
surjith-netroxe <surjith.k@netroxe.com>
2025-10-24 11:25:35 +0530

    FAQ updated

--- components/sections/Common/FAQ.tsx
+++ components/sections/Common/FAQ.tsx
@@ -43,8 +43,8 @@ function FAQ() {
     },
   ];
   return (
-    <section className="py-8 md:py-12 px-4 md:px-8 lg:px-16 bg-white mb-20">
-      <div className="max-w-7xl mx-auto">
+    <section className="py-8 md:py-12 px-20 bg-white mb-20">
+      <div className="max-w-full mx-auto">
         <div className="mb-12">
           <h2 className="text-3xl md:text-4xl font-bold text-black mb-4">
             <TTSWrapper text="Frequently Asked Questions">
